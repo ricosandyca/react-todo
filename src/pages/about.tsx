800279190016cd18ba00e22a6d74ad90180507aa
Rico Sandyca Novenza <ricosandyca.if@gmail.com>
2020-01-01 00:55:44 +0700

    Add about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import {
   Container,
-  Grid
+  Grid,
+  Divider,
+  Typography,
+  Link,
+  Box,
+  IconButton,
+  Tooltip
 } from '@material-ui/core'
+import {
+  Favorite,
+  Code,
+  GitHub
+} from '@material-ui/icons'
 
 import {
   header as Header
@@ -11,10 +22,36 @@ import {
 class AboutPage extends React.Component {
   render () {
     return (
-      <Container>
+      <Container className='about'>
         <Grid container justify='center'>
           <Grid item xs={12} md={8} lg={6}>
             <Header />
+            <Divider />
+            <Box className='github' my={3}>
+              <Tooltip title='Fork me!'>
+                <Link
+                  href='https://github.com/ricosandyca/react-todo'
+                  target='_blank'>
+                  <IconButton>
+                    <GitHub className='button' />
+                  </IconButton>
+                </Link>
+              </Tooltip>
+            </Box>
+            <Typography variant='subtitle1' color='textPrimary'>
+              <span className='footer'>
+                <Code fontSize='small' color='inherit' />
+                &nbsp;with&nbsp;
+                <Favorite fontSize='small' color='secondary' />
+                &nbsp;by&nbsp;
+                <Link
+                  href='https://github.com/ricosandyca'
+                  target='_blank'
+                  color='secondary'>
+                  ricosandyca
+                </Link>
+              </span>
+            </Typography>
           </Grid>
         </Grid>
       </Container>
