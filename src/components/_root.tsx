7d6e7b38f03655a12a9b2519eff99db57da9b311
Rico Sandyca Novenza <ricosandyca.if@gmail.com>
2020-01-09 18:27:54 +0700

    Restore: Dynamic meta theme-color

--- src/components/_root.tsx
+++ src/components/_root.tsx
@@ -10,15 +10,10 @@ type TProps =
 
 const root: React.FC<TProps> = props => {
   const { darkTheme } = props.option
-  const metaThemeColor = document.querySelector('meta[name=theme-color]')
   if (darkTheme) {
     document.body.classList.add('dark-theme')
-    if (metaThemeColor)
-      metaThemeColor.setAttribute('content', '#000000')
   } else {
     document.body.classList.remove('dark-theme')
-    if (metaThemeColor)
-      metaThemeColor.setAttribute('content', '#ffffff')
   }
 
   return null
